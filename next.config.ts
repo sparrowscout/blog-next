1b449b2b9d448f0fcd0bd52c4604c944855ee1f3
김민경 <105103167+sparrowscout@users.noreply.github.com>
2025-08-11 11:41:23 +0900

    add: setting svg -> components (add next-svgr, @svgr/webpack, update next.config.ts, add assets)

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,58 @@ const nextConfig: NextConfig = {
   compiler: {
     styledComponents: true,
   },
+  experimental: {
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
+      },
+    },
+  },
+  webpack: (config) => {
+    const fileLoaderRule = config.module.rules.find(
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-expect-error
+      (rule) => rule.test?.test?.('.svg'),
+    )
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/,
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+
+        resourceQuery: {
+          not: [...fileLoaderRule.resourceQuery.not, /url/],
+        },
+        use: [
+          {
+            loader: '@svgr/webpack',
+            options: {
+              typescript: true,
+              ext: 'tsx',
+              icon: true,
+              dimensions: false,
+              svgo: true,
+              svgoConfig: {
+                plugins: [
+                  { name: 'removeViewBox', active: false }, // 절대 끄기!
+                ],
+              },
+            },
+          },
+        ],
+      },
+    )
+    fileLoaderRule.exclude = /\.svg$/i
+    return config
+  },
 }
 
 export default nextConfig

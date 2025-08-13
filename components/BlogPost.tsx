9b6e08c2532772f014fd176119c2b82fbe1d6138
김민경 <105103167+sparrowscout@users.noreply.github.com>
2025-08-13 10:30:57 +0900

    feat: 블로그형 포스트 컴포넌트 업데이트 (검색 결과 화면에도 쓰려고)

---
+++ components/BlogPost.tsx
@@ -0,0 +1,45 @@
+import { PostMeta } from '@/types/postData.types'
+import Link from 'next/link'
+import styled from 'styled-components'
+import CategoryTag from './CategoryTag'
+import { formatSmartDate } from '@/utils/formatDate'
+import useCategoryListStore from '@/store/useCategoryListStore'
+
+interface BlogPostProps {
+  post: PostMeta
+}
+
+export default function BlogPost({ post }: BlogPostProps) {
+  const { categoryList } = useCategoryListStore()
+  const category = post.category || 'Uncategorized'
+
+  return (
+    <Link href={`/${post.slug}`} key={post.slug}>
+      <div className="flex flex-col gap-2 border-[1px] border-black bg-white p-5 hover:[&_div]:!text-[#ff01ff] active:[&_div]:!text-[#ff01ff]">
+        <div className="flex items-center justify-between">
+          <div className="text-lg ">{post.title}</div>
+
+          <DateTag>{formatSmartDate(post.date)}</DateTag>
+        </div>
+
+        <CategoryTag
+          categoryName={post.category ?? ''}
+          categoryColor={
+            categoryList.get(category)?.color ||
+            'transparent'
+          }
+        />
+      </div>
+    </Link>
+  )
+}
+
+const DateTag = styled.div`
+  display: flex;
+  padding: 4px;
+  width: max-content;
+  white-space: nowrap;
+  justify-content: center;
+  background-color: transparent;
+  border: #000 solid 1px;
+`

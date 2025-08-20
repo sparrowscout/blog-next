ed84d23f5d377c54b9b861045f7fc15337f7237d
김민경 <105103167+sparrowscout@users.noreply.github.com>
2025-08-20 15:07:27 +0900

    feat: 블로그 내용 페이지에 to top 플로팅 버튼 추가

--- components/common/ClientScrollContainer.tsx
+++ components/common/ClientScrollContainer.tsx
@@ -1,11 +1,12 @@
-// components/ClientScrollContainer.tsx
 'use client'
 import * as React from 'react'
 import { useIsIOSChrome } from '@/hooks/useIsIOSChrome'
+import IconButton from './IconButton'
+import ArrowIcon from '@/assets/icons/arrow_with_tail.svg'
 
 type Props = React.PropsWithChildren<{
   className?: string
-  paddingValue?: number // 필요시 인라인 보정
+  paddingValue?: number
 }>
 
 export default function ClientScrollContainer({
@@ -13,6 +14,9 @@ export default function ClientScrollContainer({
   children,
   paddingValue,
 }: Props) {
+  const [isToTopBtnShowing, setIsToTopBtnShowing] =
+    React.useState<boolean>(false)
+  const scrollContainer = React.useRef<HTMLDivElement>(null)
   const isIOSChrome = useIsIOSChrome()
   // iOS Chrome(오버레이)면 패딩 X, 그 외에는 패딩 추가
   const cls = `${className ?? ''} ${isIOSChrome ? '' : 'pr-2'}`
@@ -24,8 +28,38 @@ export default function ClientScrollContainer({
       }
     : undefined
 
+  React.useEffect(() => {
+    const container = scrollContainer.current
+    if (!container) return
+
+    container.onscroll = () => {
+      if (container.scrollTop > 120) {
+        setIsToTopBtnShowing(true)
+      } else setIsToTopBtnShowing(false)
+    }
+  }, [scrollContainer.current])
+
+  const onClickToTop = () => {
+    const container = scrollContainer.current
+    if (!container) return
+
+    container.scrollTo({ top: 0, behavior: 'smooth' })
+  }
+
   return (
-    <div className={cls} style={style}>
+    <div
+      className={cls}
+      style={style}
+      ref={scrollContainer}
+    >
+      <div
+        className={`fixed -right-0  bottom-10   ${isToTopBtnShowing ? '-translate-x-10 ' : 'translate-x-40'}  transition-transform ease-in-out`}
+        onClick={onClickToTop}
+      >
+        <IconButton>
+          <ArrowIcon style={{ rotate: '90deg' }} />
+        </IconButton>
+      </div>
       {children}
     </div>
   )

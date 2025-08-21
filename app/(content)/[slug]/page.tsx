acf8994738f0324f74cbc1a512040ba8e6f5740b
김민경 <105103167+sparrowscout@users.noreply.github.com>
2025-08-21 17:11:52 +0900

    feat: 포스트 내용에 카테고리, 날짜, hr 추가

--- app/(content)/[slug]/page.tsx
+++ app/(content)/[slug]/page.tsx
@@ -6,6 +6,7 @@ import rehypePrism from 'rehype-prism-plus'
 import fs from 'node:fs/promises'
 import { mdxComponents } from '@/mdx-components'
 import ClientScrollContainer from '@/components/common/ClientScrollContainer'
+import { formatDate } from 'date-fns'
 
 export async function generateStaticParams() {
   const posts = await getAllPosts()
@@ -45,11 +46,17 @@ export default async function PostPage({
   return (
     <ClientScrollContainer className="h-dvh select-text overflow-scroll px-3 pb-36 pt-32 !leading-relaxed">
       <article className="m-auto box-border max-w-[1280px] border-[1px] border-gray-900 bg-white p-4">
+        <div>{meta.category} /</div>
+
         <h1>
           {String(
             frontmatter?.title ?? meta.title ?? meta.slug,
           )}
         </h1>
+        <div className="text-end text-sm">
+          {formatDate(meta.date, 'yyyy/MM/dd')}
+        </div>
+        <hr className="!mb-12" />
         {content}
       </article>
     </ClientScrollContainer>
